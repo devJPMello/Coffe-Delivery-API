6d5125a3d01674582c5602e3b5f30dd249c8af2f
DevJPMello <joaopedromendesmello@gmail.com>
2025-06-11 19:37:23 -0300

    Atividade Prática – CoffeeDelivery (Parte 2)

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Post,
   Get,
+  Delete,
   Param,
   Body,
   HttpCode,
@@ -14,22 +15,35 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 export class CoffeeController {
   constructor(private readonly coffeeService: CoffeeService) {}
 
-  // POST /coffees
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() dto: CreateCoffeeDto) {
     return this.coffeeService.create(dto);
   }
 
-  // GET /coffees
   @Get()
-  getAll() {
+  findAll() {
     return this.coffeeService.findAll();
   }
 
-  // GET /coffees/:id
   @Get(':id')
-  getById(@Param('id') id: string) {
+  findById(@Param('id') id: string) {
     return this.coffeeService.findById(Number(id));
+  }
+
+  @Get(':id/order')
+  findPedidosByCafeId(@Param('id') id: string) {
+    return this.coffeeService.findPedidosByCafeId(Number(id));
+  }
+
+  @Get('plus-order-coffee')
+  findMaisVendidos() {
+    return this.coffeeService.findMaisVendidos();
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.coffeeService.remove(Number(id));
   }
 }
